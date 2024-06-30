c95920c73e5dee04a3492f68e0a0be243f8b73d7
Mohammad Zain <mohammad.zain575@gmail.com>
2024-06-30 12:16:54 +0400

    Ready to Deploy tailwind1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../frontend/src/**/*.js"],
+  content: ["../frontend/src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
